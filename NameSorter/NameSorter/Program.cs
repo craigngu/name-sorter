6010264bbc0a232135c3894f81f842012d35bd2d
Craig Ngu <craig.ngu@gmail.com>
2018-08-14 21:41:57 +1000

    Check in solution

---
+++ NameSorter/NameSorter/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace NameSorter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var serviceProvider = SetupServiceProvider();
+
+            var logger = serviceProvider.GetService<ILogger<Program>>();
+
+            logger.LogInformation("Application Started");
+
+            logger.LogInformation("Application Ended");
+
+            Console.ReadKey();
+        }
+
+        private static ServiceProvider SetupServiceProvider()
+        {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            return serviceCollection.BuildServiceProvider();
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging(configure => configure.AddConsole());
+        }
+    }
+}
